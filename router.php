dbd0aae29847ed89888d4b88fe4d1c71244bd2e0
jose-acosta-git <jose.acosta.git@gmail.com>
2020-10-02 10:43:55 -0300

    se agrego .htaccess y router

---
+++ router.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'app/controllers/review.controller.php';
+
+
+// defino la base url para la construccion de links con urls semánticas
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+// lee la acción
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home'; // acción por defecto si no envían
+}
+
+// parsea la accion Ej: suma/1/2 --> ['suma', 1, 2]
+$params = explode('/', $action);
+
+// determina que camino seguir según la acción
+switch ($params[0]) {
+    case 'home':
+        $controller = new ReviewController();
+        $controller->showHome();
+        break;
+    case 'listar':
+        $controller = new ReviewController();
+        $controller->showReviews();
+        break;
+    case 'filtrar':
+        
+    default:
+        header("HTTP/1.0 404 Not Found");
+        echo('404 Page not found');
+        break;
+}
