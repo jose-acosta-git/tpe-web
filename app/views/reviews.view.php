c82d23cda2c365ccd19f0386fe3b1709fd1766f2
jose-acosta-git <jose.acosta.git@gmail.com>
2020-10-03 10:56:44 -0300

    se agrego view de reviews

---
+++ app/views/reviews.view.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'libs/smarty/libs/Smarty.class.php';   
+
+class ReviewsView {
+    
+    function printReviews($reviews) {
+
+    }
+
+    function printReview($review) {
+
+    }
+
+    function showError($msg) {
+        
+    }
+
+}
